18ad9c1f28f4535154dd88d0d96263474701433e
nmovalle <11480516cii@gmail.com>
2024-06-27 09:02:17 -0600

    add first and last name columns for engineer skills entity

--- TheCollabSys.Backend.Entity/DTOs/EngineerSkillDetailDTO.cs
+++ TheCollabSys.Backend.Entity/DTOs/EngineerSkillDetailDTO.cs
@@ -4,6 +4,8 @@ public class EngineerSkillDetailDTO
 {
     public int EngineerId { get; set; }
     public string EngineerName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
 
     public List<SkillLevelDTO> Skills { get; set; }
 }
